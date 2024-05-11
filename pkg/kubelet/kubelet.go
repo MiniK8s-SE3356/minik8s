f6f756abe962e714cd9b7a4049942dd6d8ba4820
LeviXubbbb <levi.xu.bin@gmail.com>
2024-05-11 20:37:51 +0800

    kubelet minor

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -2,8 +2,10 @@ package kubelet
 
 import (
 	minik8s_types "github.com/MiniK8s-SE3356/minik8s/pkg/types"
+	minik8s_message "github.com/MiniK8s-SE3356/minik8s/pkg/utils/message"
 )
 
 type Kubelet struct {
 	kubeletConfiguration *minik8s_types.KubeletConfiguration
+	mqConn               *minik8s_message.MQConnection
 }
