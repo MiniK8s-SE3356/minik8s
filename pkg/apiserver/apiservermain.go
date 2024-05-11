8b6b30a895395b2431cf865351cddbcb58e0ea9b
CoderHaiqin <3567840474@qq.com>
2024-05-11 00:49:50 -0700

    merge feature/etcdclient/dyh

--- pkg/apiserver/apiservermain.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/MiniK8s-SE3356/minik8s/pkg/apiserver/server"
-
-func main() {
-	server.Start()
-}
--- pkg/apiserver/process/process.go
+++
@@ -1,5 +0,0 @@
-package process
-
-import "github.com/MiniK8s-SE3356/minik8s/pkg/etcdclient"
-
-var EtcdCli *etcdclient.EtcdClient
