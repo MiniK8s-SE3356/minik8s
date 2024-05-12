94f52cd57f4f8084652c1ec786b1514f2d3c6665
CoderHaiqin <3567840474@qq.com>
2024-05-12 04:27:56 -0700

    feat: idgenerate uuid

--- pkg/utils/idgenerate/idgenerate.go
+++ pkg/utils/idgenerate/idgenerate.go
@@ -1,10 +1,7 @@
 package idgenerate
 
 import (
-	"fmt"
-	"math/rand"
-	"strings"
-	"time"
+	"github.com/google/uuid"
 )
 
 const (
@@ -14,22 +11,25 @@ const (
 )
 
 func GenerateID() (string, error) {
-	rand.NewSource(time.Now().UnixNano())
+	// rand.NewSource(time.Now().UnixNano())
 
-	// 定义字符池，包含数字和字母
-	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	// // 定义字符池，包含数字和字母
+	// const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-	// 生成长度为8的随机ID
-	var id strings.Builder
-	for i := 0; i < 8; i++ {
-		// 随机选择字符池中的一个字符
-		index := rand.Int() % len(chars)
-		err := id.WriteByte(chars[index])
-		if err != nil {
-			fmt.Println("failed to write to id string")
-			return "", err
-		}
-	}
+	// // 生成长度为8的随机ID
+	// var id strings.Builder
+	// for i := 0; i < 8; i++ {
+	// 	// 随机选择字符池中的一个字符
+	// 	index := rand.Int() % len(chars)
+	// 	err := id.WriteByte(chars[index])
+	// 	if err != nil {
+	// 		fmt.Println("failed to write to id string")
+	// 		return "", err
+	// 	}
+	// }
 
-	return id.String(), nil
+	// return id.String(), nil
+	id := uuid.New().String()
+
+	return id, nil
 }
