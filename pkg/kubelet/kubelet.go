c39daf48d8ba2bea777018b153d325ae0de6a43c
LeviXubbbb <levi.xu.bin@gmail.com>
2024-05-13 15:38:14 +0800

    minor

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -1,10 +1,19 @@
 package kubelet
 
 import (
+	kubelet_worker "github.com/MiniK8s-SE3356/minik8s/pkg/kubelet/worker"
 	minik8s_message "github.com/MiniK8s-SE3356/minik8s/pkg/utils/message"
 )
 
 type Kubelet struct {
-	kubeletConfiguration *KubeletConfiguration
-	mqConn               *minik8s_message.MQConnection
+	kubeletConfig *KubeletConfig
+	mqConn        *minik8s_message.MQConnection
+	podManager    *kubelet_worker.PodManager
+}
+
+func NewKubelet(config *KubeletConfig) *Kubelet {
+	kubelet := &Kubelet{
+		kubeletConfig: config,
+	}
+	return kubelet
 }
