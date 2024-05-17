9cb395dbf438e1a26d8372ff335dd741e7179e30
CoderHaiqin <3567840474@qq.com>
2024-05-17 02:53:37 -0700

    添加replicasetController

--- pkg/controller/app/controller.go
+++ pkg/controller/app/controller.go
@@ -6,13 +6,15 @@ import (
 
 	"github.com/MiniK8s-SE3356/minik8s/pkg/controller/endpointsController"
 	"github.com/MiniK8s-SE3356/minik8s/pkg/controller/nodesController"
+	replicasetcontroller "github.com/MiniK8s-SE3356/minik8s/pkg/controller/replicasetController"
 	"github.com/MiniK8s-SE3356/minik8s/pkg/controller/servicesController"
 )
 
 type Controller struct {
-	endpointsController *(endpointsController.EndpointsController)
-	nodesController     *(nodesController.NodesController)
-	servicesController  *(servicesController.ServicesController)
+	endpointsController  *(endpointsController.EndpointsController)
+	nodesController      *(nodesController.NodesController)
+	servicesController   *(servicesController.ServicesController)
+	replicasetController *(replicasetcontroller.ReplicasetController)
 }
 
 func NewController() *(Controller) {
@@ -20,10 +22,12 @@ func NewController() *(Controller) {
 	endpoints_controller := endpointsController.NewEndpointsController()
 	nodes_controller := nodesController.NewNodesController()
 	services_controller := servicesController.NewServicesController()
+	replicaset_controller := replicasetcontroller.NewReplicasetController()
 	return &Controller{
-		endpointsController: endpoints_controller,
-		nodesController:     nodes_controller,
-		servicesController:  services_controller,
+		endpointsController:  endpoints_controller,
+		nodesController:      nodes_controller,
+		servicesController:   services_controller,
+		replicasetController: replicaset_controller,
 	}
 }
 
@@ -39,6 +43,7 @@ func (co *Controller) Run() {
 	go co.endpointsController.Run()
 	go co.nodesController.Run()
 	go co.servicesController.Run()
+	go co.replicasetController.Run()
 
 	time.Sleep(2 * time.Second)
 }
---
+++ pkg/controller/replicasetController/replicasetController.go
@@ -0,0 +1,118 @@
+package replicasetcontroller
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/MiniK8s-SE3356/minik8s/pkg/apiObject/pod"
+	"github.com/MiniK8s-SE3356/minik8s/pkg/apiObject/replicaset"
+	"github.com/MiniK8s-SE3356/minik8s/pkg/apiserver/url"
+	"github.com/MiniK8s-SE3356/minik8s/pkg/utils/httprequest"
+)
+
+type ReplicasetController struct {
+}
+
+func NewReplicasetController() *(ReplicasetController) {
+	fmt.Printf("New ReplicasetController...\n")
+	return &ReplicasetController{}
+}
+
+func checkMatchedPod(podLabels map[string]string, selector map[string]string) bool {
+	for k, v := range podLabels {
+		if selector[k] == v {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getPodsFromServer() ([]pod.Pod, error) {
+	var result []pod.Pod
+
+	jsonData, err := httprequest.GetRequest(url.GetPodURL)
+	if err != nil {
+		fmt.Println("error in get request")
+		return result, err
+	}
+
+	err = json.Unmarshal([]byte(jsonData), &result)
+	if err != nil {
+		fmt.Println("failed to unmarshal")
+		fmt.Println(jsonData)
+		return result, err
+	}
+
+	return result, err
+}
+
+func getReplicasetsFromServer() ([]replicaset.Replicaset, error) {
+	var result []replicaset.Replicaset
+
+	jsonData, err := httprequest.GetRequest(url.GetReplicasetURL)
+	if err != nil {
+		fmt.Println("error in get request")
+		return result, err
+	}
+
+	err = json.Unmarshal([]byte(jsonData), &result)
+	if err != nil {
+		fmt.Println("failed to unmarshal")
+		fmt.Println(jsonData)
+		return result, err
+	}
+
+	return result, err
+}
+
+func rscTask() {
+	// 从APIserver获取全体pod和全体replicaset
+	pods, err := getPodsFromServer()
+	if err != nil {
+		fmt.Println("failed to get pod from server")
+		return
+	}
+
+	replicasets, err := getReplicasetsFromServer()
+	if err != nil {
+		fmt.Println("failed to get replicaset from server")
+	}
+
+	// 利用label找到replicaset对应的pod
+	for _, rs := range replicasets {
+		var matchedPod []pod.Pod
+
+		for _, pod := range pods {
+			if checkMatchedPod(pod.Metadata.Labels, rs.Spec.Selector.MatchLabels) {
+				matchedPod = append(matchedPod, pod)
+			}
+		}
+
+		// 比对pod数量和期望数量
+
+		if len(matchedPod) > rs.Spec.Replicas {
+			// delete some pod
+		} else if len(matchedPod) < rs.Spec.Replicas {
+			// add some pod
+		}
+	}
+
+	// 处理孤儿pod
+}
+
+func (sc *ReplicasetController) Init() {
+	fmt.Printf("Init ReplicasetController ...\n")
+
+}
+
+func (sc *ReplicasetController) Run() {
+	fmt.Printf("Run ReplicasetController ...\n")
+
+	for {
+		time.After(10 * time.Second)
+
+		rscTask()
+	}
+}
