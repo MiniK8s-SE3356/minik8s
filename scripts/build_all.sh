19d483bba3b2a4bcd147d59e63c09030daf1bfa4
CoderHaiqin <3567840474@qq.com>
2024-05-22 01:39:20 -0700

    fix: build script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,4 +1,4 @@
 go build -o build/scheduler ./pkg/scheduler/main
-go build -o build/apiserver ./pkg/apiserver
-go build -o build/kubectl ./pkg/kubectl
+go build -o build/apiserver ./cmd/apiserver
+go build -o build/kubectl ./cmd/kubectl
 go build -o build/controller ./cmd/controller
