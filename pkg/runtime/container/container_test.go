6766235d436b9d9e842365f26d4540f7e91e2a8b
LeviXubbbb <levi.xu.bin@gmail.com>
2024-05-25 21:08:14 +0800

    minor

--- pkg/runtime/container/container_test.go
+++ pkg/runtime/container/container_test.go
@@ -1,14 +1,11 @@
 package container_test
 
 import (
-	"fmt"
 	"testing"
-
-	runtime_container "github.com/MiniK8s-SE3356/minik8s/pkg/runtime/container"
 )
 
 func TestMain(m *testing.M) {
-	containerManager := &runtime_container.ContainerManager{}
+	// containerManager := &runtime_container.ContainerManager{}
 	// config := &minik8s_container.CreateContainerConfig{
 	// 	Image: "nginx:alpine",
 	// }
@@ -45,15 +42,15 @@ func TestMain(m *testing.M) {
 	// fmt.Println(id)
 
 	//....test get container by name...//
-	container, err := containerManager.GetContainerByName("chf")
-	if err != nil {
-		fmt.Println("Error getting containers")
-		panic(err)
-	}
-	if container == nil {
-		fmt.Println("Container not found")
-		return
-	}
-	fmt.Println(container.ID)
-	fmt.Println(container.State)
+	// container, err := containerManager.GetContainerByName("chf")
+	// if err != nil {
+	// 	fmt.Println("Error getting containers")
+	// 	panic(err)
+	// }
+	// if container == nil {
+	// 	fmt.Println("Container not found")
+	// 	return
+	// }
+	// fmt.Println(container.ID)
+	// fmt.Println(container.State)
 }
--- pkg/utils/cAdvisorUtils/cAdvisor_test.go
+++ pkg/utils/cAdvisorUtils/cAdvisor_test.go
@@ -2,20 +2,18 @@ package cadvisorutils_test
 
 import (
 	"testing"
-
-	cadvisorutils "github.com/MiniK8s-SE3356/minik8s/pkg/utils/cAdvisorUtils"
 )
 
 func TestMain(m *testing.M) {
-	cadvisorutils.GetMachineInfo()
-	// cadvisorutils.GetContainerInfo(
+	// cadvisorutils.GetMachineInfo()
+	// // cadvisorutils.GetContainerInfo(
+	// // 	"localhost",
+	// // 	"8080",
+	// // 	"some-rabbit",
+	// // )
+	// cadvisorutils.GetContainerCPUandMemory(
 	// 	"localhost",
-	// 	"8080",
+	// 	"8090",
 	// 	"some-rabbit",
 	// )
-	cadvisorutils.GetContainerCPUandMemory(
-		"localhost",
-		"8090",
-		"some-rabbit",
-	)
 }
