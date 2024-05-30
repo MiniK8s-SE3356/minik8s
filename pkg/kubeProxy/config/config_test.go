7f3eb326f84bf98d099b8f77ba0928416ad2f3cb
Rebornix-zero <1980250928@qq.com>
2024-05-31 00:49:02 +0800

    test: init test structure for kubeProxy

---
+++ pkg/kubeProxy/config/config_test.go
@@ -0,0 +1,18 @@
+package config_test
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// pre-test code
+
+	// test func
+	exitCode := m.Run()
+
+	// post-test code
+
+	// 返回测试运行的退出码
+	os.Exit(exitCode)
+}
