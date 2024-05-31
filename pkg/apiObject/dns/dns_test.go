4c3716ee184f5e53b9eddaa565c9f998928eb7dc
Rebornix-zero <1980250928@qq.com>
2024-05-31 08:26:30 +0800

    test: minor

--- pkg/apiObject/dns/dns_test.go
+++ pkg/apiObject/dns/dns_test.go
@@ -3,10 +3,31 @@ package dns_test
 import (
 	"os"
 	"testing"
+
+	"github.com/MiniK8s-SE3356/minik8s/pkg/apiObject/dns"
+	"github.com/google/uuid"
 )
+
+var dns_object dns.Dns
 
 func TestMain(m *testing.M) {
 	// pre-test code
+	dns_object.ApiVersion = "v1"
+	dns_object.Kind = "Dns"
+	dns_object.Metadata = dns.DnsMetadata{
+		Name:      "test-dns1",
+		Namespace: "default",
+		Id:        uuid.NewString(),
+	}
+	dns_object.Spec = dns.DnsSpec{
+		Host:  "dns.test",
+		Paths: []dns.DnsPathInfo{},
+	}
+	dns_object.Status = dns.DnsStatus{
+		Phase:       dns.DNS_NOTREADY,
+		Version:     0,
+		PathsStatus: make(map[string]dns.DnsPathStatus),
+	}
 
 	// test func
 	exitCode := m.Run()
@@ -17,6 +38,8 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func TestXxx(t *testing.T) {
-
+func TestDns(t *testing.T) {
+	if dns_object.ApiVersion != "v1" || dns_object.Kind != "Dns" || dns_object.Metadata.Name != "test-dns1" || dns_object.Spec.Host != "dns.test" || dns_object.Status.Phase != dns.DNS_NOTREADY {
+		t.Fatalf("Dns test error, can't match the init field value.\n")
+	}
 }
